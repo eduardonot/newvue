65379d22ab24c674f48d31356bdbfc06c276dd71
eduardonot <eduardo_not@hotmail.com>
2021-08-25 16:20:46 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login </router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view class="container"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    
+    html, body{
+        padding: 0 !important;
+        margin: 0 !important;
+        height: 100% !important;
+    }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .container{
+        width: 100%;
+        margin:0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #nav {
+        padding: 30px;
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template lang="">
+    <div>
+        <div class="main">
+            <div id="login-form">
+                <div id="login-logo">
+
+                </div>
+                <div class="btn-select btn-login">login</div>
+                <div class="btn-select btn-cadastrar">cadastrar</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    .main{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 10px;
+    }
+
+    .btn-select{
+        padding: 10px;
+        margin-bottom: 10px;
+        width: 80%;
+    }
+
+    .btn-login{
+        background-color: rgba(107,179,246,1);
+        text-transform: uppercase;
+    }
+
+    .btn-cadastrar{
+        text-transform: uppercase;
+        background-color: rgba(245,88,88,1);
+    }
+    
+    #login-form{
+        width: 15%;
+        margin:0;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 2px 4px 10px 0px rgba(0,0,0,0.75);
+        -webkit-box-shadow: 2px 4px 10px 0px rgba(0,0,0,0.75);
+        -moz-box-shadow: 2px 4px 10px 0px rgba(0,0,0,0.75);
+    }
+
+    #login-logo{
+        background-color: rgba(245,88,88,1);
+        width: 150px;
+        height: 150px;
+        margin-bottom: 100px;
+        margin-top: 10px;
+    }
+</style>
